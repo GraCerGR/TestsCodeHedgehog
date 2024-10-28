8a239956e3f7c5e24f7e55be7ef63b682e4ae18f
Grigoryy <ctrhtnysq.afqk@gmail.com>
2024-10-28 18:58:30 +0700

    Сделал юнит тесты для всех доступных функций

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from settings import *
 import unittest
 
 from Class import *
@@ -14,7 +15,7 @@ from Queue import *
 class TestQueue(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
-        login_to_profile(self.browser,"https://dev.code.kupriyanov.space/", "ctrhtnysq.afqk@gmail.com", "Ro91684912")
+        login_to_profile(self.browser,SITELINK, USERNAME, PASSWORD)
         login_to_class(self.browser, 'test')
 
     def test_posts(self):
