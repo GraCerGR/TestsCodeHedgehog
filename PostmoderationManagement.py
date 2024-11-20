69de97af88561303ef9a2af0a6bd86751842b032
Grigoryy <ctrhtnysq.afqk@gmail.com>
2024-11-20 15:18:06 +0700

    Сделал автотест просмотра тестов Начал делать автотест просмотра деталей тестов

--- PostmoderationManagement.py
+++ PostmoderationManagement.py
@@ -1,3 +1,4 @@
+import random
 from time import sleep
 
 from selenium.common.exceptions import TimeoutException, NoSuchElementException, InvalidSelectorException
@@ -21,6 +22,11 @@ def postmoderation_management(browser, verdict):
     #if not making_a_solution_for_tests(browser):
     #    return False
 
+    if not go_to_the_postmoderation_tab(browser):
+        return False
+    if not viewing_tests(browser):
+        return False
+    return False
     # Переотправку решения делаю в очереди, т. к. предсозданная задача с автоматической установкой вердикта
     if not go_to_the_queue_tab(browser):
         return False
@@ -38,6 +44,8 @@ def postmoderation_management(browser, verdict):
         return False
 
     if not go_to_the_postmoderation_tab(browser):
+        return False
+    if not viewing_tests(browser):
         return False
     if not issuing_a_verdict(browser, verdict):
         return False
@@ -433,6 +441,130 @@ def issuing_a_delete_verdict(browser):
     printSuccess(f"Удаление вердикта работает")
     return True
 
+# Просмотр тестов
+def viewing_tests(browser):
+    solutiondata = searchElementOfTable(browser)
+    if not solutiondata:
+        printExeption(f"Решения не найдены")
+        return False
+    try:
+        attempt = WebDriverWait(browser, 20).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR,
+                                            '.ant-table-row.ant-table-row-level-0'))
+        )
+    except (TimeoutException, NoSuchElementException):
+        printExeption(f"Строка с попыткой не найдена")
+        return False
+    except Exception as e:
+        # Выводим тип ошибки и сообщение
+        printExeption(f"Тип ошибки: {type(e).__name__}")
+        printExeption(f"Сообщение ошибки: {e}")
+
+    try:
+        buttons_attempt = attempt.find_elements(By.CSS_SELECTOR, '.ant-btn.css-14h5sa0.ant-btn-text.ant-btn-lg.ant-btn-icon-only.IconButton_icon_button__7vyd9')
+        if len(buttons_attempt) != 2:
+            printExeption("Кнопка деталей решения не найдена или кол-во кнопок не равно 2")
+            return False
+        buttons_attempt[0].click()
+    except (TimeoutException, NoSuchElementException):
+        printExeption(f"Кнопка деталей решения не найдена")
+        return False
+    except Exception as e:
+        # Выводим тип ошибки и сообщение
+        printExeption(f"Тип ошибки: {type(e).__name__}")
+        printExeption(f"Сообщение ошибки: {e}")
+
+    sleep(0.5)
+
+# -------------- Проверка первого поля тестов --------------
+    try:
+        testField = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "//div[contains(@class, 'TestCasesOverview_test_cases_overview__9c6ww')]"))
+        )
+
+        acceptButtons = testField.find_elements(By.XPATH,".//button[contains(@class, 'TestCasesOverview_tooltip_success__8AUpi')]")
+        errorButtons = testField.find_elements(By.XPATH,".//button[contains(@class, 'TestCasesOverview_tooltip_error__c0+yt')]")
+        emptyButtons = testField.find_elements(By.XPATH,".//div[contains(@class, 'TestCasesOverview_test_case_item_stub__ogiAI')]")
+
+
+        acceptButtons_count = len(acceptButtons)
+        errorButtons_count = len(errorButtons)
+        emptyButtons_count = len(emptyButtons)
+
+        printInfo(f"(Первое поле) Количество пройденных тестов: {acceptButtons_count}")
+        printInfo(f"(Первое поле) Количество проваленных тестов: {errorButtons_count}")
+        printInfo(f"(Первое поле) Общее число квадратов: {emptyButtons_count + errorButtons_count + acceptButtons_count}")
+
+    except (TimeoutException, NoSuchElementException):
+        printExeption(f"Область отображения тестов или сами тесты не найдены")
+        return False
+    except Exception as e:
+        # Выводим тип ошибки и сообщение
+        printExeption(f"Тип ошибки: {type(e).__name__}")
+        printExeption(f"Сообщение ошибки: {e}")
+
+# -------------- Проверка второго поля тестов --------------
+    try:
+        testButton = WebDriverWait(browser, 10).until(
+            EC.visibility_of_element_located((By.XPATH,
+                                              "//div[contains(@class, 'ant-collapse-header') and .//span[text()='Развернуть список тестов']]"))
+        )
+        testButton.click()
+    except (TimeoutException, NoSuchElementException):
+        printExeption(f"Меню тестов не найдено")
+        return False
+    except Exception as e:
+        printExeption(f"Тип ошибки: {type(e).__name__}")
+        printExeption(f"Сообщение ошибки: {e}")
+        return False
+
+    try:
+        table_container = WebDriverWait(browser, 10).until(
+            EC.visibility_of_element_located(
+                (By.XPATH,
+                 "//span[text()='Развернуть список тестов']/ancestor::div[contains(@class, 'ant-collapse-item')]"))
+        )
+        sleep(0.5)
+        allTests = table_container.find_elements(By.CSS_SELECTOR, ".ant-table-tbody tr")
+        acceptedTests = table_container.find_elements(By.XPATH, "//tr[td/div/p[text()='Accepted']]")
+        errorTests = table_container.find_elements(By.XPATH, "//tr[td/div[contains(@class, 'Verdict_verdict_red__zOVn1')]]")
+
+        allTests_count = len(allTests)
+        acceptedTests_count = len(acceptedTests)
+        errorTests_count = len(errorTests)
+
+        printInfo(f"(Второе поле) Количество пройденных тестов: {acceptedTests_count}")
+        printInfo(f"(Второе поле) Количество проваленных тестов: {errorTests_count}")
+        printInfo(f"(Второе поле) Общее число тестов: {allTests_count}")
+
+    except (TimeoutException, NoSuchElementException):
+        printExeption(f"Меню тестов не найдено")
+        return False
+    except Exception as e:
+        printExeption(f"Тип ошибки: {type(e).__name__}")
+        printExeption(f"Сообщение ошибки: {e}")
+        return False
+
+    printSuccess(f"Просмотр тестов решения функционирует")
+
+# -------------- Проверка деталей тестов --------------
+    try:
+        allButtons = acceptButtons + errorButtons
+        if allButtons:
+            random.choice(allButtons).click()
+        else:
+            printExeption(f"Кнопок тестов не существует")
+            return False
+    except (TimeoutException, NoSuchElementException):
+        printExeption(f"Меню тестов не найдено")
+        return False
+    except Exception as e:
+        printExeption(f"Тип ошибки: {type(e).__name__}")
+        printExeption(f"Сообщение ошибки: {e}")
+        return False
+
+    sleep(0.5)
 
 def searchElementOfTable(browser):
     try:
@@ -475,13 +607,12 @@ def searchElementOfTable(browser):
             verdict=cells[4].text,
         )
 
-        data = {
-            'user': solutiondata.user,
-            'task_name': solutiondata.task_name,
-            'language': solutiondata.language,
-            'submission_date': solutiondata.submission_date,
-            'verdict': solutiondata.verdict,
-        }
+        data = f"""
+                user: {solutiondata.user}, 
+                task_name: {solutiondata.task_name}, 
+                language: {solutiondata.language}, 
+                submission_date: {solutiondata.submission_date}, 
+                verdict: {solutiondata.verdict.replace("\n", ", ")}"""
 
         printInfo(f"Данные решения найдены: {data}")
         return solutiondata
